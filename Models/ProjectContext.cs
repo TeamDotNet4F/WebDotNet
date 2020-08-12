7c3e2b41a3550bc2f0f23434ac9e1c9bf434ad5e
ThanhTung212Z <tung016485@gmail.com>
2020-08-12 11:23:02 +0700

    Model added

---
+++ Models/ProjectContext.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Project.Models.DAO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProjectCore.Models
+{
+    public class ProjectContext : DbContext
+    {
+        public DbSet<sanpham> Sanpham { get; set; }
+        public DbSet<thongso> Thongso { get; set; }
+        public DbSet<khuyenmai> KhuyenMai { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseMySQL("server=localhost;database=project_dotnet;user=root;password=StrongPass123");
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<sanpham>(entity =>
+           {
+               entity.HasKey(e => e.idsanpham);
+               entity.Property(e => e.idthongso).IsRequired();
+               entity.HasOne(p => p.khuyenmai).WithOne(d => d.sanpham);
+           });
+            modelBuilder.Entity<khuyenmai>(entity =>
+            {
+                entity.HasKey(e => e.idKhuyenMai);
+            });
+        }
+    }
+}
